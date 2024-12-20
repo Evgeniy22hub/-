52a3f11953a7ccdf6130f67455ef3556eaf48d48
Evgeniy22hub <gogus6541@gmail.com>
2024-12-20 23:21:38 +0300

    ЛАБ 1 задание 3

---
+++ LAB 1.2.c
@@ -0,0 +1,102 @@
+#include "stm32f1xx_hal.h"
+
+#define LED1_PIN                               GPIO_PIN_9
+#define LED2_PIN                               GPIO_PIN_8
+#define LED3_PIN                               GPIO_PIN_7
+#define LED4_PIN                               GPIO_PIN_6
+
+#define LED5_PIN                               GPIO_PIN_5
+
+
+
+#define LED_GPIO_PORT                          GPIOB
+
+
+#define LED_GPIO_CLK_ENABLE()                 __HAL_RCC_GPIOB_CLK_ENABLE()
+
+int main(void)
+{
+  HAL_Init();
+
+  LED_GPIO_CLK_ENABLE();
+
+  GPIO_InitTypeDef GPIO_InitStruct;
+
+  GPIO_InitStruct.Pin = LED1_PIN | LED2_PIN | LED3_PIN | LED4_PIN;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+  HAL_GPIO_Init(LED_GPIO_PORT, &GPIO_InitStruct);
+
+  
+  GPIO_InitStruct.Pin = LED5_PIN;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+
+  long frequency = 1000;  
+  
+  while (1){
+    if(HAL_GPIO_ReadPin(LED_GPIO_PORT,LED5_PIN) == GPIO_PIN_SET){
+        HAL_GPIO_TogglePin(LED_GPIO_PORT, LED1_PIN);
+        HAL_Delay(frequency);
+        HAL_GPIO_TogglePin(LED_GPIO_PORT, LED1_PIN);
+        HAL_GPIO_TogglePin(LED_GPIO_PORT, LED2_PIN);
+        HAL_Delay(frequency);
+        HAL_GPIO_TogglePin(LED_GPIO_PORT, LED2_PIN);
+        HAL_GPIO_TogglePin(LED_GPIO_PORT, LED3_PIN);
+        HAL_Delay(frequency);
+        HAL_GPIO_TogglePin(LED_GPIO_PORT, LED3_PIN);
+        HAL_GPIO_TogglePin(LED_GPIO_PORT, LED4_PIN);
+        HAL_Delay(frequency);
+        HAL_GPIO_TogglePin(LED_GPIO_PORT, LED4_PIN); 
+
+        frequency = frequency == 100 ? 1000 : frequency - 100;
+    }
+  }
+}
+
+void SysTick_Handler(void)
+{
+  HAL_IncTick();
+}
+
+void NMI_Handler(void)
+{
+}
+
+void HardFault_Handler(void)
+{
+  while (1) {}
+}
+
+
+void MemManage_Handler(void)
+{
+  while (1) {}
+}
+
+void BusFault_Handler(void)
+{
+  while (1) {}
+}
+
+void UsageFault_Handler(void)
+{
+  while (1) {}
+}
+
+void SVC_Handler(void)
+{
+}
+
+
+void DebugMon_Handler(void)
+{
+}
+
+void PendSV_Handler(void)
+{
+}
